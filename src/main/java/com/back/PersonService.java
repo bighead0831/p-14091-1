41fd52ebad421162e2582f7a13f2d2f20fd27719
bighead0831 <bighead0831@naver.com>
2026-05-11 12:41:51 +0900

    4강 생성하는 객체가 단순할때는 Component를 사용하자. (Bean은 생성 객체가 복잡할 때 유용하다)

--- src/main/java/com/back/PersonService.java
+++ src/main/java/com/back/PersonService.java
@@ -1,6 +1,9 @@
 
 package com.back;
 
+import org.springframework.stereotype.Service;
+
+@Service  // 생성하는 객체가 단순할때는 Component를 사용하자. (Bean은 생성 객체가 복잡할 때 유용하다)
 public class PersonService {
     public long count() {
         return 3;
